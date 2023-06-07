9a53df28c0d5464a72f599ebefd60ddfc9899c63
Aleksandr Yakovlev <yak0vlev.as@yandex.ru>
2023-06-07 17:17:44 +0300

    ТЗ11:Первая попытка теста

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.yandex.practicum.filmorate.service.InterfaceService.InterfaceServiceFilm;
